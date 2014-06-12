0cb2dc4e05cee8c9c25ccb276d290c4f261b1620
木村圭佑 <muxukeikei0227@gmail.com>
2014-06-12 14:47:17 +0900

    fix code format

--- Quick/Core/NSString+QCKSelectorName.m
+++ Quick/Core/NSString+QCKSelectorName.m
@@ -15,13 +15,23 @@
     static NSMutableCharacterSet *invalidCharacters = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        invalidCharacters = [NSMutableCharacterSet whitespaceAndNewlineCharacterSet];
+        invalidCharacters = [NSMutableCharacterSet new];
         
-        [invalidCharacters formUnionWithCharacterSet:[NSCharacterSet illegalCharacterSet]];
-        [invalidCharacters formUnionWithCharacterSet:[NSCharacterSet controlCharacterSet]];
-        [invalidCharacters formUnionWithCharacterSet:[NSCharacterSet punctuationCharacterSet]];
-        [invalidCharacters formUnionWithCharacterSet:[NSCharacterSet nonBaseCharacterSet]];
-        [invalidCharacters formUnionWithCharacterSet:[NSCharacterSet symbolCharacterSet]];
+        NSCharacterSet *whitespaceCharacterSet = [NSCharacterSet whitespaceCharacterSet];
+        NSCharacterSet *newlineCharacterSet = [NSCharacterSet newlineCharacterSet];
+        NSCharacterSet *illegalCharacterSet = [NSCharacterSet illegalCharacterSet];
+        NSCharacterSet *controlCharacterSet = [NSCharacterSet controlCharacterSet];
+        NSCharacterSet *punctuationCharacterSet = [NSCharacterSet punctuationCharacterSet];
+        NSCharacterSet *nonBaseCharacterSet = [NSCharacterSet nonBaseCharacterSet];
+        NSCharacterSet *symbolCharacterSet = [NSCharacterSet symbolCharacterSet];
+
+        [invalidCharacters formUnionWithCharacterSet:whitespaceCharacterSet];
+        [invalidCharacters formUnionWithCharacterSet:newlineCharacterSet];
+        [invalidCharacters formUnionWithCharacterSet:illegalCharacterSet];
+        [invalidCharacters formUnionWithCharacterSet:controlCharacterSet];
+        [invalidCharacters formUnionWithCharacterSet:punctuationCharacterSet];
+        [invalidCharacters formUnionWithCharacterSet:nonBaseCharacterSet];
+        [invalidCharacters formUnionWithCharacterSet:symbolCharacterSet];
     });
     
     NSArray *validComponents = [self componentsSeparatedByCharactersInSet:invalidCharacters];
