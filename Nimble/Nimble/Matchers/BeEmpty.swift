6b2deb3e06bd251ca75bbf7d2d455b48f03aea5f
endersstocker <bryan@lanausee.com>
2014-06-23 10:26:12 -0400

    Added beEmpty() Nimble matcher

---
+++ Nimble/Nimble/Matchers/BeEmpty.swift
@@ -0,0 +1,57 @@
+//
+//  BeEmpty.swift
+//  Quick
+//
+//  Created by Bryan Enders on 6/23/14.
+//
+
+import Foundation
+
+class BeEmpty: Matcher {
+    init() {
+        super.init(0)
+    }
+    
+    let nilMessage = "expected subject not to be nil"
+    
+    override func failureMessage(actual: NSObject?) -> String {
+        return actual
+            ? "expected subject to be empty, got '\(_flatten(actual))'"
+            : nilMessage
+    }
+    
+    override func negativeFailureMessage(actual: NSObject?) -> String {
+        return actual
+            ? "expected subject not to be empty"
+            : nilMessage
+    }
+    
+    override func match(actual: NSObject?) -> Bool {
+        if let array = actual as? NSArray {
+            return array.count == expected
+        } else if let set = actual as? NSSet {
+            return set.count == expected
+        } else if let string = actual as? NSString {
+            return string.length == expected
+        } else {
+            return false
+        }
+    }
+    
+    func _flatten(collection: NSObject?) -> String {
+        if let array = collection as? NSArray {
+            return "[ " + array.componentsJoinedByString(", ") + " ]"
+        } else if let set = collection as? NSSet {
+            let array = set.allObjects as NSArray
+            return "[ " + array.componentsJoinedByString(", ") + " ]"
+        } else {
+            return "\(collection)"
+        }
+    }
+}
+
+extension Prediction {
+    func beEmpty() {
+        evaluate(BeEmpty())
+    }
+}
