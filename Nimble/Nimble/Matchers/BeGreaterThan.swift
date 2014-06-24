84ae1bd6245da89df46e0cd9c1ec139147cb4493
endersstocker <bryan@lanausee.com>
2014-06-24 10:26:27 -0400

    [Nimble] Added beLessThanOrEqualTo(), beGreaterThan() & beGreaterThanOrEqualTo() matchers

---
+++ Nimble/Nimble/Matchers/BeGreaterThan.swift
@@ -0,0 +1,39 @@
+//
+//  BeGreaterThan.swift
+//  Nimble
+//
+//  Created by Bryan Enders on 6/24/14.
+//
+
+import Foundation
+
+class BeGreaterThan: Matcher {
+    let _nilMessage = "expected subject to not be nil"
+    
+    override func failureMessage(actual: NSObject?) -> String {
+        return actual
+            ? "expected subject to be > '\(expected)', got '\(actual)'"
+            : _nilMessage
+    }
+    
+    override func negativeFailureMessage(actual: NSObject?) -> String {
+        return actual
+            ? "expected subject to not be > '\(expected)', got '\(actual)'"
+            : _nilMessage
+    }
+    
+    override func match(actual: NSObject?) -> Bool {
+        if let actualNumber = actual as? NSNumber {
+            if let expectedNumber = expected as? NSNumber {
+                return actualNumber.compare(expectedNumber) == NSComparisonResult.OrderedDescending
+            }
+        }
+        return false
+    }
+}
+
+extension Prediction {
+    func beGreaterThan(expected: NSObject?) {
+        evaluate(BeGreaterThan(expected))
+    }
+}
