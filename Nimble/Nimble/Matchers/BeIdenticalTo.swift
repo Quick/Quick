1015248c46ab6636c13472863e3aa5f6e758f2a6
Alex Basson <alexbasson@gmail.com>
2014-06-26 22:03:29 -0400

    Revise failure messages to print pointer addresses

--- Nimble/Nimble/Matchers/BeIdenticalTo.swift
+++ Nimble/Nimble/Matchers/BeIdenticalTo.swift
@@ -10,11 +10,11 @@ import Foundation
 
 class BeIdenticalTo: Matcher {
     override func failureMessage(actual: NSObject?) -> String {
-        return "expected '\(expected)' to be the same instance as '\(actual)'"
+        return NSString(format: "expected '%@' (%p) to be identical to '%@' (%p)", actual!, actual!, expected!, expected!)
     }
 
     override func negativeFailureMessage(actual: NSObject?) -> String {
-        return "expected subject not to be the same instance as '\(expected)'"
+        return NSString(format: "expected '%@' (%p) not to be identical to '%@' (%p)", actual!, actual!, expected!, expected!)
     }
 
     override func match(actual: NSObject?) -> Bool {
