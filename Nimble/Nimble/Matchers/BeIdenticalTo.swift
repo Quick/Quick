385ac1b092d49097095ae69a8dbc7677004f4a31
Alex Basson <alexbasson@gmail.com>
2014-06-29 09:28:13 -0400

    Don't force-unwrap variables; revise failure messages

--- Nimble/Nimble/Matchers/BeIdenticalTo.swift
+++ Nimble/Nimble/Matchers/BeIdenticalTo.swift
@@ -10,11 +10,19 @@ import Foundation
 
 class BeIdenticalTo: Matcher {
     override func failureMessage(actual: NSObject?) -> String {
-        return NSString(format: "expected '%@' (%p) to be identical to '%@' (%p)", actual!, actual!, expected!, expected!)
+        if let unwrappedExpected = expected {
+            if let unwrappedActual = actual {
+                return NSString(format: "expected '%@' (%p), got '%@' (%p)", unwrappedActual, unwrappedActual, unwrappedExpected, unwrappedExpected)
+            } else {
+                return NSString(format: "expected '%@' (%p), got nil", unwrappedExpected, unwrappedExpected)
+            }
+        } else {
+            return NSString(format: "cannot expect nil to be identical to anything")
+        }
     }
 
     override func negativeFailureMessage(actual: NSObject?) -> String {
-        return NSString(format: "expected '%@' (%p) not to be identical to '%@' (%p)", actual!, actual!, expected!, expected!)
+        return NSString(format: "expected subject not to be identical to '%@' (%p)", expected!, expected!)
     }
 
     override func match(actual: NSObject?) -> Bool {
