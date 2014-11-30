e1d9b0d2a9ed2244509a43d18ddeeefe8c437361
Brian Gesiak <bgesiak@fb.com>
2014-11-30 09:38:29 -0500

    AfterEachTests: Test order
    Functional tests for afterEach tested the total number of times
    afterEach closures were called, but not the order in which they were
    called. Test the order as well.
    
    This is brittle in that these tests may break when test order shuffling
    is introduced (see #152)--but when that time comes, shuffling should
    simply be disabled when running these order-based tests.

--- QuickTests/FunctionalTests/AfterEachTests.swift
+++ QuickTests/FunctionalTests/AfterEachTests.swift
@@ -2,23 +2,51 @@ import XCTest
 import Quick
 import Nimble
 
-var outerAfterEachExecutedCount = 0
-var innerAfterEachExecutedCount = 0
-var noExamplesAfterEachExecutedCount = 0
+private enum AfterEachType {
+    case OuterOne
+    case OuterTwo
+    case OuterThree
+    case InnerOne
+    case InnerTwo
+    case NoExamples
+}
+
+private var afterEachOrder = [AfterEachType]()
 
 class FunctionalTests_AfterEachSpec: QuickSpec {
     override func spec() {
-        beforeEach { outerAfterEachExecutedCount += 1 }
-        it("executes the outer afterEach once") {}
-        it("executes the outer afterEach a second time") {}
+        afterEach { afterEachOrder.append(AfterEachType.OuterOne) }
+        afterEach { afterEachOrder.append(AfterEachType.OuterTwo) }
+        afterEach { afterEachOrder.append(AfterEachType.OuterThree) }
 
-        context("when there are nested beforeEach") {
-            beforeEach { innerAfterEachExecutedCount += 1 }
-            it("executes the outer and inner afterEach") {}
+        it("executes the outer afterEach closures once, but not before this closure [1]") {
+            // No examples have been run, so no afterEach will have been run either.
+            // The list should be empty.
+            expect(afterEachOrder).to(beEmpty())
+        }
+
+        it("executes the outer afterEach closures a second time, but not before this closure [2]") {
+            // The afterEach for the previous example should have been run.
+            // The list should contain the afterEach for that example, executed from top to bottom.
+            expect(afterEachOrder).to(equal([AfterEachType.OuterOne, AfterEachType.OuterTwo, AfterEachType.OuterThree]))
+        }
+
+        context("when there are nested afterEach") {
+            afterEach { afterEachOrder.append(AfterEachType.InnerOne) }
+            afterEach { afterEachOrder.append(AfterEachType.InnerTwo) }
+
+            it("executes the outer and inner afterEach closures, but not before this closure [3]") {
+                // The afterEach for the previous two examples should have been run.
+                // The list should contain the afterEach for those example, executed from top to bottom.
+                expect(afterEachOrder).to(equal([
+                    AfterEachType.OuterOne, AfterEachType.OuterTwo, AfterEachType.OuterThree,
+                    AfterEachType.OuterOne, AfterEachType.OuterTwo, AfterEachType.OuterThree,
+                ]))
+            }
         }
 
         context("when there are nested afterEach without examples") {
-            beforeEach { noExamplesAfterEachExecutedCount += 1 }
+            afterEach { afterEachOrder.append(AfterEachType.NoExamples) }
         }
     }
 }
@@ -26,30 +54,26 @@ class FunctionalTests_AfterEachSpec: QuickSpec {
 class AfterEachTests: XCTestCase {
     override func setUp() {
         super.setUp()
-        outerAfterEachExecutedCount = 0
-        innerAfterEachExecutedCount = 0
-        noExamplesAfterEachExecutedCount = 0
+        afterEachOrder = []
     }
 
     override func tearDown() {
-        outerAfterEachExecutedCount = 0
-        innerAfterEachExecutedCount = 0
-        noExamplesAfterEachExecutedCount = 0
+        afterEachOrder = []
         super.tearDown()
     }
 
-    func testOuterAfterEachIsExecutedOnceAfterEachExample() {
+    func testAfterEachIsExecutedInTheCorrectOrder() {
         qck_runSpec(FunctionalTests_AfterEachSpec.classForCoder())
-        XCTAssertEqual(outerAfterEachExecutedCount, 3)
-    }
-
-    func testInnerAfterEachIsExecutedOnceAfterEachInnerExample() {
-        qck_runSpec(FunctionalTests_AfterEachSpec.classForCoder())
-        XCTAssertEqual(innerAfterEachExecutedCount, 1)
-    }
-
-    func testNoExamplesAfterEachIsNeverExecuted() {
-        qck_runSpec(FunctionalTests_AfterEachSpec.classForCoder())
-        XCTAssertEqual(noExamplesAfterEachExecutedCount, 0)
+        let expectedOrder = [
+            // [1] The outer afterEach closures are executed from top to bottom.
+            AfterEachType.OuterOne, AfterEachType.OuterTwo, AfterEachType.OuterThree,
+            // [2] The outer afterEach closures are executed from top to bottom.
+            AfterEachType.OuterOne, AfterEachType.OuterTwo, AfterEachType.OuterThree,
+            // [3] The inner afterEach closures are executed from top to bottom,
+            //     then the outer afterEach closures are executed from top to bottom.
+            AfterEachType.InnerOne, AfterEachType.InnerTwo,
+                AfterEachType.OuterOne, AfterEachType.OuterTwo, AfterEachType.OuterThree,
+        ]
+        XCTAssertEqual(afterEachOrder, expectedOrder)
     }
 }
