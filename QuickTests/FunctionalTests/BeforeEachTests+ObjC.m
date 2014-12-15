91ed4c1a9cb4ed75f9131b09a0e95c42623c75b3
Markus Kauppila <markus.kauppila@gmail.com>
2014-12-15 19:00:54 +0200

    Add test for BeforeEachSpec

--- QuickTests/FunctionalTests/BeforeEachTests+ObjC.m
+++ QuickTests/FunctionalTests/BeforeEachTests+ObjC.m
@@ -4,6 +4,16 @@
 
 #import "QCKSpecRunner.h"
 
+typedef NS_ENUM(NSUInteger, BeforeEachType) {
+    OuterOne,
+    OuterTwo,
+    InnerOne,
+    InnerTwo,
+    InnerThree,
+    NoExamples,
+};
+
+static NSMutableArray *beforeEachOrder;
 
 QuickSpecBegin(FunctionalTests_BeforeEachSpec);
 QuickSpecEnd;
@@ -13,14 +23,28 @@ QuickSpecEnd;
 @implementation BeforeEachTests
 
 - (void)setUp {
+    beforeEachOrder = [NSMutableArray array];
     [super setUp];
 }
 
 - (void)tearDown {
+    beforeEachOrder = [NSMutableArray array];
     [super tearDown];
 }
 
 - (void)testBeforeEachIsExecutedInTheCorrectOrder {
+    qck_runSpec([FunctionalTests_BeforeEachSpec class]);
+    NSArray *expectedOrder = @[
+        // [1] The outer beforeEach closures are executed from top to bottom.
+        @(OuterOne), @(OuterTwo),
+        // [2] The outer beforeEach closures are executed from top to bottom.
+        @(OuterOne), @(OuterTwo),
+        // [3] The outer beforeEach closures are executed from top to bottom,
+        //     then the innter beforeEach closures are executed from top to bottom.
+        @(OuterOne), @(OuterTwo), @(InnerOne), @(InnerTwo), @(InnerThree),
+    ];
+
+    XCTAssertEqualObjects(beforeEachOrder, expectedOrder);
 }
 
 @end
