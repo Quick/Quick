5141ceb193edb1ba9caa94ad6ce7eb90f5bb962f
Markus Kauppila <markus.kauppila@gmail.com>
2015-01-04 20:58:23 +0200

    Implement AfterSuiteTests for Objective-C

--- QuickTests/FunctionalTests/AfterSuiteTests+ObjC.m
+++ QuickTests/FunctionalTests/AfterSuiteTests+ObjC.m
@@ -4,12 +4,21 @@
 
 #import "QCKSpecRunner.h"
 
+static BOOL afterSuiteWasExecuted = NO;
+
 QuickSpecBegin(FunctionalTests_AfterSuite_AfterSuiteSpec)
+
+afterSuite(^{
+    afterSuiteWasExecuted = YES;
+});
 
 QuickSpecEnd
 
-
 QuickSpecBegin(FunctionalTests_AfterSuite_Spec)
+
+it(@"is executed before afterSuite", ^{
+    expect(@(afterSuiteWasExecuted)).to(beFalsy());
+});
 
 QuickSpecEnd
 
@@ -18,7 +27,16 @@ QuickSpecEnd
 @implementation AfterSuiteTests
 
 - (void)testAfterSuiteIsNotExecutedBeforeAnyExamples {
+    // Execute the spec with an assertion after the one with an afterSuite.
+    NSArray *specs = @[
+        [FunctionalTests_AfterSuite_AfterSuiteSpec class],
+        [FunctionalTests_AfterSuite_Spec class]
+    ];
+    XCTestRun *result = qck_runSpecs(specs);
 
+    // Although this ensures that afterSuite is not called before any
+    // examples, it doesn't test that it's ever called in the first place.
+    XCTAssert(result.hasSucceeded);
 }
 
 @end
