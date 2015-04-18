aaa9e994bc8dfc642672bbef4e2b694db27ab0ac
Assaf Gelber <assaf.gelber@gmail.com>
2015-04-18 15:37:28 +0300

    Add basic test

--- QuickTests/FunctionalTests/DefinitionsTests.swift
+++ QuickTests/FunctionalTests/DefinitionsTests.swift
@@ -1,9 +1,35 @@
-//
-//  DefinitionsTests.swift
-//  Quick
-//
-//  Created by Assaf Gelber on 4/18/15.
-//  Copyright (c) 2015 Brian Ivan Gesiak. All rights reserved.
-//
+import XCTest
+import Quick
+import Nimble
 
-import Foundation
+private var fetches = [Int]()
+
+class FunctionalTests_DefinitionsSpec: QuickSpec {
+    override func spec() {
+        define("one") { return 1 }
+
+        it("should evaluate and return the defined variable") {
+            let one = fetch("one") as! Int
+            fetches.append(one)
+            expect(one).to(equal(1))
+        }
+    }
+}
+
+class DefinitionsTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        fetches = []
+    }
+    
+    override func tearDown() {
+        fetches = []
+        super.tearDown()
+    }
+    
+    func testFetchesReturnExpectedValues() {
+        qck_runSpec(FunctionalTests_DefinitionsSpec)
+        let expectedFetches = [1]
+        XCTAssertEqual(fetches, expectedFetches)
+    }
+}
