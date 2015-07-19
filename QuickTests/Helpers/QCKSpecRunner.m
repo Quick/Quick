a67d4ca077778f05792803d16d45da9064001dcd
Jeff Hui <jeff@jeffhui.net>
2015-07-19 13:24:29 -0700

    Resolve warnings

--- QuickTests/Helpers/QCKSpecRunner.m
+++ QuickTests/Helpers/QCKSpecRunner.m
@@ -8,7 +8,15 @@ XCTestRun *qck_runSuite(XCTestSuite *suite) {
 
     __block XCTestRun *result = nil;
     [[XCTestObservationCenter sharedTestObservationCenter] _suspendObservationForBlock:^{
-        result = [suite run];
+        if ([suite respondsToSelector:@selector(runTest)]) {
+            [suite runTest];
+            result = suite.testRun;
+        } else {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+            result = [suite run];
+#pragma clang diagnostic pop
+        }
     }];
     return result;
 }
