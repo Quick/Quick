764c9cfe8fa1e621c760ed1e5f1156893859d3b6
Jeff Hui <jeff@jeffhui.net>
2015-10-24 20:52:06 -0700

    Use relative header imports instead of absolute
    ...to a framework module name.

--- Quick/Quick.h
+++ Quick/Quick.h
@@ -6,8 +6,6 @@ FOUNDATION_EXPORT double QuickVersionNumber;
 //! Project version string for Quick.
 FOUNDATION_EXPORT const unsigned char QuickVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Quick/PublicHeader.h>
-
-#import <Quick/QuickSpec.h>
-#import <Quick/QCKDSL.h>
-#import <Quick/QuickConfiguration.h>
+#import "QuickSpec.h"
+#import "QCKDSL.h"
+#import "QuickConfiguration.h"
