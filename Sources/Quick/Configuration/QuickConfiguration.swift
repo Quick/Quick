11d7109864dbe09b2c71b4e77dcfcc2c52cfd3d2
Brian Croom <bcroom@pivotal.io>
2015-12-17 05:16:35 +0000

    Add pure swift QuickSpec and QuickConfiguration classes
    These are intended to be used in environments without Objective-C

---
+++ Sources/Quick/Configuration/QuickConfiguration.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+// NOTE: This file is not intended to be included in the Xcode project. It
+//       is picked up by the Swift Package Manager during its build process.
+
+public class QuickConfiguration {
+    public class func configure(configuration: Configuration) {}
+}
