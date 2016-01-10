5e60c49a4c3e67bada29561fd4dde03938bcc172
Adam Sharp <adsharp@me.com>
2016-01-11 08:52:33 +1100

    Add remaining documentation for selected test suite builder

--- Quick/NSBundle+CurrentTestBundle.swift
+++ Quick/NSBundle+CurrentTestBundle.swift
@@ -2,6 +2,9 @@ import Foundation
 
 extension NSBundle {
 
+    /**
+     Locates the first bundle with a '.xctest' file extension.
+     */
     internal static var currentTestBundle: NSBundle? {
         return allBundles().lazy
             .filter {
--- Quick/QuickSelectedTestSuiteBuilder.swift
+++ Quick/QuickSelectedTestSuiteBuilder.swift
@@ -1,11 +1,34 @@
+/**
+ Responsible for building a "Selected tests" suite. This corresponds to a single
+ spec, and all its examples.
+ */
 internal class QuickSelectedTestSuiteBuilder: QuickTestSuiteBuilder {
 
+    /**
+     The test spec class to run.
+     */
     let testCaseClass: AnyClass!
 
+    /**
+     For Objective-C classes, returns the class name. For Swift classes without,
+     an explicit Objective-C name, returns a module-namespaced class name
+     (e.g., "FooTests.FooSpec").
+     */
     var namespacedClassName: String {
         return NSStringFromClass(testCaseClass)
     }
 
+    /**
+     Given a test case name:
+
+        FooSpec/testFoo
+
+     Optionally constructs a test suite builder for the named test case class
+     in the running test bundle.
+
+     If no test bundle can be found, or the test case class can't be found,
+     initialization fails and returns `nil`.
+     */
     init?(forTestCaseWithName name: String) {
         guard let testCaseClass = testCaseClassForTestCaseWithName(name) else {
             self.testCaseClass = nil
@@ -15,12 +38,21 @@ internal class QuickSelectedTestSuiteBuilder: QuickTestSuiteBuilder {
         self.testCaseClass = testCaseClass
     }
 
+    /**
+     Returns a `QuickTestSuite` that runs the associated test case class.
+     */
     func buildTestSuite() -> QuickTestSuite {
         return QuickTestSuite(forTestCaseClass: testCaseClass)
     }
 
 }
 
+/**
+ Searches `NSBundle.allBundles()` for an xctest bundle, then looks up the named
+ test case class in that bundle.
+
+ Returns `nil` if a bundle or test case class cannot be found.
+ */
 private func testCaseClassForTestCaseWithName(name: String) -> AnyClass? {
     func extractClassName(name: String) -> String? {
         return name.characters.split("/").first.map(String.init)
