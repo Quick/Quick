fc7d379f9d89d055f2b16a11f8c2821e62f277a1
Brian Croom <bcroom@pivotal.io>
2016-01-16 08:47:34 +0000

    Add QuickTestHelpers package with the qck_runSpec function
    Includes a TestRun struct mirroring XCTestRun properties

--- Package.swift
+++ Package.swift
@@ -3,8 +3,9 @@ import PackageDescription
 let package = Package(
     name: "Quick",
     targets: [
-        Target(name: "QuickTests", dependencies: [.Target(name: "Quick")]),
-        Target(name: "QuickFocusedTests", dependencies: [.Target(name: "Quick")])
+        Target(name: "QuickTests", dependencies: [.Target(name: "Quick"), .Target(name: "QuickTestHelpers")]),
+        Target(name: "QuickFocusedTests", dependencies: [.Target(name: "Quick"), .Target(name: "QuickTestHelpers")]),
+        Target(name: "QuickTestHelpers", dependencies: [.Target(name: "Quick")]),
     ],
     dependencies: [ // TODO: This ought to be `testDependencies:` but there seems to be a swiftpm bug causing the lib to not get linked then
         .Package(url: "https://github.com/briancroom/Nimble", majorVersion: 3)
