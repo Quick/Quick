365fdf6f032d0bb0d848c65419665a84797b7bfb
Brian Croom <brian.s.croom@gmail.com>
2016-01-16 03:05:23 -0500

    Update Package.swift to specify the tests' dependency on Nimble

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
 import PackageDescription
 
 let package = Package(
-  name: "Quick"
+    name: "Quick",
+    targets: [
+        Target(name: "QuickTests", dependencies: [.Target(name: "Quick")]),
+        Target(name: "QuickFocusedTests", dependencies: [.Target(name: "Quick")])
+    ],
+    dependencies: [ // TODO: This ought to be `testDependencies:` but there seems to be a swiftpm bug causing the lib to not get linked then
+        .Package(url: "https://github.com/briancroom/Nimble", majorVersion: 3)
+    ]
 )
