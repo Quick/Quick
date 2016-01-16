2ec73b0071eaa87d3bc39ded6ab32e00f2b1a460
Brian Croom <bcroom@pivotal.io>
2016-01-16 18:21:07 +0000

    Update TODO comment in Package.swift for future reference

--- Package.swift
+++ Package.swift
@@ -7,7 +7,10 @@ let package = Package(
         Target(name: "QuickFocusedTests", dependencies: [.Target(name: "Quick"), .Target(name: "QuickTestHelpers")]),
         Target(name: "QuickTestHelpers", dependencies: [.Target(name: "Quick")]),
     ],
-    dependencies: [ // TODO: This ought to be `testDependencies:` but there seems to be a swiftpm bug causing the lib to not get linked then
+    // TODO: Once the `test` command has been implemented in the Swift Package Manager, this should be changed to
+    // be `testDependencies:` instead. For now it has to be done like this for the library to get linked with the test targets.
+    // See: https://github.com/apple/swift-evolution/blob/master/proposals/0019-package-manager-testing.md
+    dependencies: [ 
         .Package(url: "https://github.com/briancroom/Nimble", majorVersion: 3)
     ]
 )
