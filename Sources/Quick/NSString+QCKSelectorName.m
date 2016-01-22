5537ac96835403c95f24b9fc38c1831a45e992db
Dov Frankel <dov@abbey-code.com>
2016-01-22 11:30:59 -0500

    Reverted changes to qck_selectorName extension method

--- Sources/Quick/NSString+QCKSelectorName.m
+++ Sources/Quick/NSString+QCKSelectorName.m
@@ -4,12 +4,8 @@
 
 - (NSString *)qck_selectorName {
     static NSMutableCharacterSet *invalidCharacters = nil;
-    static NSString *separator = @"_";
-    static NSCharacterSet *separatorSet = nil;
     static dispatch_once_t onceToken;
-    
     dispatch_once(&onceToken, ^{
-        separatorSet = [NSCharacterSet characterSetWithCharactersInString:separator];
         invalidCharacters = [NSMutableCharacterSet new];
 
         NSCharacterSet *whitespaceCharacterSet = [NSCharacterSet whitespaceCharacterSet];
@@ -31,13 +27,7 @@
 
     NSArray *validComponents = [self componentsSeparatedByCharactersInSet:invalidCharacters];
 
-    NSString *selector = [validComponents componentsJoinedByString:separator];
-    selector = [selector stringByTrimmingCharactersInSet:separatorSet];
-    if ([selector length] == 0) {
-        selector = @"blank";
-    }
-
-    return selector;
+    return [validComponents componentsJoinedByString:@"_"];
 }
 
 @end
