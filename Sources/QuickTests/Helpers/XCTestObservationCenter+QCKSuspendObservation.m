1445ba1658685c4c724f7151a91d63ea2e13f89b
Brian Croom <brian.s.croom@gmail.com>
2016-03-02 13:10:58 +0100

    Cleanups to the observation suspension utility

--- Sources/QuickTests/Helpers/XCTestObservationCenter+QCKSuspendObservation.m
+++ Sources/QuickTests/Helpers/XCTestObservationCenter+QCKSuspendObservation.m
@@ -7,6 +7,10 @@
 
 @implementation XCTestObservationCenter (QCKSuspendObservation)
 
+/// This allows us to only suspend observation for observers by provided by Apple
+/// as a part of the XCTest framework. In particular it is important that we not
+/// suspend the observer added by Nimble, otherwise it is unable to properly
+/// report assertion failures.
 static BOOL (^isFromApple)(id, BOOL *) = ^BOOL(id observer, BOOL *stop){
     return [[NSBundle bundleForClass:[observer class]].bundleIdentifier containsString:@"com.apple.dt.XCTest"];
 };
@@ -15,9 +19,12 @@ static BOOL (^isFromApple)(id, BOOL *) = ^BOOL(id observer, BOOL *stop){
     NSSet *observersToSuspend = [[self observers] objectsPassingTest:isFromApple];
     [[self observers] minusSet:observersToSuspend];
 
-    block();
-
-    [[self observers] unionSet:observersToSuspend];
+    @try {
+        block();
+    }
+    @finally {
+        [[self observers] unionSet:observersToSuspend];
+    }
 }
 
 @end
