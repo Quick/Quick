7c73b0155767eeb6d9d02a30a6af2d9e4800c321
Fujiki Takeshi <Takeshi.Fujiki@jp.sony.com>
2016-05-23 12:08:56 +0900

    rephrase documentation for pending, excluded

--- Sources/Quick/Filter.swift
+++ Sources/Quick/Filter.swift
@@ -22,17 +22,24 @@ final public class Filter: NSObject {
     }
 
     /**
-        Example and example groups with [Excluded: true] are excluded from test runs.
-        Use this to temporarily suspend examples that you know do not pass yet.
+        Example and example groups with [Excluded: true] are not included in test runs.
+        Use this to disable examples that do not pass. Of course, you could also comment
+        the examples out altogether, but excluding them will ensure they continue to at
+        least compile, which allows them to be re-enabled more easily in the future.
+     
+        In addition, future versions of Quick may be able to point out when a disabled test
+        passes, and thus should be re-enabled.
     */
     public class var excluded: String {
         return "excluded"
     }
 
     /**
-     Example and example groups with [Pending: true] are pending from test runs.
-     Use this to temporarily suspend examples that you know do not pass yet.
-     */
+        Like "excluded", example and example groups with [Pending: true] are not included in test runs,
+        and so you can use this to temporarily suspend examples that you know do not pass yet.
+        "Pending" examples imply that the examples will soon pass, they just need a little more work.
+        Functionally, they are no different from "excluded" examples.
+    */
     public class var pending: String {
         return "pending"
     }
