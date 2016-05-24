a5953f404ca886b0f7b92f0213e7accabeb6d5bf
takecian <fujikidev@gmail.com>
2016-05-24 10:24:48 +0900

    Remove extra space

--- Sources/QuickTests/FunctionalTests/PendingTests.swift
+++ Sources/QuickTests/FunctionalTests/PendingTests.swift
@@ -13,13 +13,13 @@ class FunctionalTests_PendingSpec: QuickSpec {
         xit("an example that will not run") {
             expect(true).to(beFalsy())
         }
-        
+
         describe("a describe block containing only one enabled example") {
             beforeEach { oneExampleBeforeEachExecutedCount += 1 }
             it("an example that will run") {}
             pending("an example that will not run") {}
         }
-        
+
         describe("a describe block containing only pending examples") {
             beforeEach { onlyPendingExamplesBeforeEachExecutedCount += 1 }
             pending("an example that will not run") {}
@@ -35,22 +35,22 @@ class PendingTests: XCTestCase, XCTestCaseProvider {
             ("testBeforeEachDoesNotRunForContextsWithOnlyPendingExamples", testBeforeEachDoesNotRunForContextsWithOnlyPendingExamples),
         ]
     }
-    
+
     func testAnOtherwiseFailingExampleWhenMarkedPendingDoesNotCauseTheSuiteToFail() {
         let result = qck_runSpec(FunctionalTests_PendingSpec.self)
         XCTAssert(result.hasSucceeded)
     }
-    
+
     func testBeforeEachOnlyRunForEnabledExamples() {
         oneExampleBeforeEachExecutedCount = 0
-        
+
         qck_runSpec(FunctionalTests_PendingSpec.self)
         XCTAssertEqual(oneExampleBeforeEachExecutedCount, 2)
     }
-    
+
     func testBeforeEachDoesNotRunForContextsWithOnlyPendingExamples() {
         onlyPendingExamplesBeforeEachExecutedCount = 0
-        
+
         qck_runSpec(FunctionalTests_PendingSpec.self)
         XCTAssertEqual(onlyPendingExamplesBeforeEachExecutedCount, 1)
     }
