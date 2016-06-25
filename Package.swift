19466bdc560d43534979e52eaeb58f0a7de97bde
Norio Nomura <norio.nomura@gmail.com>
2016-06-25 19:34:02 +0900

    Move `Sources/Quick/*.[hm]` to `Sources/QuickObjectiveC`
    Avoid `error: mixedSources("<path to Quick>/Sources/Quick")` on building by Swift Package Manager.

--- Package.swift
+++ Package.swift
@@ -2,6 +2,9 @@ import PackageDescription
 
 let package = Package(
     name: "Quick",
+    exclude: [
+      "Sources/QuickObjectiveC"
+    ],
     // TODO: Once the `test` command has been implemented in the Swift Package Manager, this should be changed to
     // be `testDependencies:` instead. For now it has to be done like this for the library to get linked with the test targets.
     // See: https://github.com/apple/swift-evolution/blob/master/proposals/0019-package-manager-testing.md
