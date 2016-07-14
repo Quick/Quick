594479916dc6f87af514e3d64635a1358f3ae904
Norio Nomura <norio.nomura@gmail.com>
2016-07-14 10:06:50 +0900

    Update Nimble in Package.swift

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
     // be `testDependencies:` instead. For now it has to be done like this for the library to get linked with the test targets.
     // See: https://github.com/apple/swift-evolution/blob/master/proposals/0019-package-manager-testing.md
     dependencies: [
-        .Package(url: "https://github.com/norio-nomura/Nimble", "5.0.0-alpha.30p1")
+        .Package(url: "https://github.com/norio-nomura/Nimble", "5.0.0-alpha.30p2")
     ]
 )
