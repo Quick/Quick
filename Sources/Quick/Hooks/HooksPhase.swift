6d7b8614a4106cba2ab73ea6c940ab5b892a5cf6
Syo Ikeda <suicaicoca@gmail.com>
2016-09-05 22:18:32 +0900

    [HooksPhase] No need to be `RawRepresentable`

--- Sources/Quick/Hooks/HooksPhase.swift
+++ Sources/Quick/Hooks/HooksPhase.swift
@@ -2,8 +2,8 @@
  A description of the execution cycle of the current example with
  respect to the hooks of that example.
  */
-internal enum HooksPhase: Int {
-    case nothingExecuted = 0
+internal enum HooksPhase {
+    case nothingExecuted
     case beforesExecuting
     case beforesFinished
     case aftersExecuting
