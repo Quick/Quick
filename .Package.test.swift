144b930b869d8e65413ecdca03f4e2fd65a7491e
Adam Sharp <adam@sharplet.me>
2017-06-02 23:10:12 -0400

    Update to Nimble v7.0.1

--- .Package.test.swift
+++ .Package.test.swift
@@ -22,7 +22,7 @@ let package = Package(
     // be `testDependencies:` instead. For now it has to be done like this for the library to get linked with the test targets.
     // See: https://github.com/apple/swift-evolution/blob/master/proposals/0019-package-manager-testing.md
     dependencies: [
-        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 6)
+        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 7)
     ],
     exclude: {
         var excludes = [
