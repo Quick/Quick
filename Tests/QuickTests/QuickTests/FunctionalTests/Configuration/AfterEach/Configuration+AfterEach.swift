11af876371faefb3053685c611faa87adbd8e205
Ewelina <eweszm@gmail.com>
2017-07-16 15:07:55 +0200

    Update property access control level

--- Tests/QuickTests/QuickTests/FunctionalTests/Configuration/AfterEach/Configuration+AfterEach.swift
+++ Tests/QuickTests/QuickTests/FunctionalTests/Configuration/AfterEach/Configuration+AfterEach.swift
@@ -1,7 +1,7 @@
 import Quick
 
 class FunctionalTestsConfigurationAfterEach: QuickConfiguration {
-	static public var wasExecuted = false
+    static var wasExecuted = false
     override class func configure(_ configuration: Configuration) {
         configuration.afterEach {
             wasExecuted = true
--- Tests/QuickTests/QuickTests/FunctionalTests/Configuration/BeforeEach/Configuration+BeforeEach.swift
+++ Tests/QuickTests/QuickTests/FunctionalTests/Configuration/BeforeEach/Configuration+BeforeEach.swift
@@ -1,7 +1,7 @@
 import Quick
 
 class FunctionalTestsConfigurationBeforeEach: QuickConfiguration {
-	static public var wasExecuted = false
+    static var wasExecuted = false
     override class func configure(_ configuration: Configuration) {
         configuration.beforeEach {
             wasExecuted = true
