51cd6f2a8b9abb89f5d945e1756037cb9b46cb30
Ewelina <eweszm@gmail.com>
2017-07-16 14:40:18 +0200

    Update enums to fix SwiftLint warnings
    Enum element names start with a lowercase character.

--- Tests/QuickTests/QuickTests/FunctionalTests/AfterEachTests.swift
+++ Tests/QuickTests/QuickTests/FunctionalTests/AfterEachTests.swift
@@ -3,12 +3,12 @@ import Quick
 import Nimble
 
 private enum AfterEachType {
-    case OuterOne
-    case OuterTwo
-    case OuterThree
-    case InnerOne
-    case InnerTwo
-    case NoExamples
+    case outerOne
+    case outerTwo
+    case outerThree
+    case innerOne
+    case innerTwo
+    case noExamples
 }
 
 private var afterEachOrder = [AfterEachType]()
@@ -16,9 +16,9 @@ private var afterEachOrder = [AfterEachType]()
 class FunctionalTests_AfterEachSpec: QuickSpec {
     override func spec() {
         describe("afterEach ordering") {
-            afterEach { afterEachOrder.append(AfterEachType.OuterOne) }
-            afterEach { afterEachOrder.append(AfterEachType.OuterTwo) }
-            afterEach { afterEachOrder.append(AfterEachType.OuterThree) }
+            afterEach { afterEachOrder.append(.outerOne) }
+            afterEach { afterEachOrder.append(.outerTwo) }
+            afterEach { afterEachOrder.append(.outerThree) }
 
             it("executes the outer afterEach closures once, but not before this closure [1]") {
                 // No examples have been run, so no afterEach will have been run either.
@@ -29,25 +29,25 @@ class FunctionalTests_AfterEachSpec: QuickSpec {
             it("executes the outer afterEach closures a second time, but not before this closure [2]") {
                 // The afterEach for the previous example should have been run.
                 // The list should contain the afterEach for that example, executed from top to bottom.
-                expect(afterEachOrder).to(equal([AfterEachType.OuterOne, AfterEachType.OuterTwo, AfterEachType.OuterThree]))
+                expect(afterEachOrder).to(equal([.outerOne, .outerTwo, .outerThree]))
             }
 
             context("when there are nested afterEach") {
-                afterEach { afterEachOrder.append(AfterEachType.InnerOne) }
-                afterEach { afterEachOrder.append(AfterEachType.InnerTwo) }
+                afterEach { afterEachOrder.append(.innerOne) }
+                afterEach { afterEachOrder.append(.innerTwo) }
 
                 it("executes the outer and inner afterEach closures, but not before this closure [3]") {
                     // The afterEach for the previous two examples should have been run.
                     // The list should contain the afterEach for those example, executed from top to bottom.
                     expect(afterEachOrder).to(equal([
-                        AfterEachType.OuterOne, AfterEachType.OuterTwo, AfterEachType.OuterThree,
-                        AfterEachType.OuterOne, AfterEachType.OuterTwo, AfterEachType.OuterThree
+                        .outerOne, .outerTwo, .outerThree,
+                        .outerOne, .outerTwo, .outerThree
                         ]))
                 }
             }
 
             context("when there are nested afterEach without examples") {
-                afterEach { afterEachOrder.append(AfterEachType.NoExamples) }
+                afterEach { afterEachOrder.append(.noExamples) }
             }
         }
 #if (os(macOS) || os(iOS) || os(tvOS) || os(watchOS)) && !SWIFT_PACKAGE
@@ -76,15 +76,14 @@ final class AfterEachTests: XCTestCase, XCTestCaseProvider {
         afterEachOrder = []
 
         qck_runSpec(FunctionalTests_AfterEachSpec.self)
-        let expectedOrder = [
+		let expectedOrder: [AfterEachType] = [
             // [1] The outer afterEach closures are executed from top to bottom.
-            AfterEachType.OuterOne, AfterEachType.OuterTwo, AfterEachType.OuterThree,
+            .outerOne, .outerTwo, .outerThree,
             // [2] The outer afterEach closures are executed from top to bottom.
-            AfterEachType.OuterOne, AfterEachType.OuterTwo, AfterEachType.OuterThree,
+            .outerOne, .outerTwo, .outerThree,
             // [3] The inner afterEach closures are executed from top to bottom,
             //     then the outer afterEach closures are executed from top to bottom.
-            AfterEachType.InnerOne, AfterEachType.InnerTwo,
-                AfterEachType.OuterOne, AfterEachType.OuterTwo, AfterEachType.OuterThree
+            .innerOne, .innerTwo, .outerOne, .outerTwo, .outerThree
         ]
         XCTAssertEqual(afterEachOrder, expectedOrder)
 
