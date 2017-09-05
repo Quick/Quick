ee9ba19b11d3731ec42847ed4412ccf9a4015b54
Sho Ikeda <suicaicoca@gmail.com>
2017-09-05 13:46:17 +0900

    [ExampleMetadata] Use `objcMembers`

--- Sources/Quick/ExampleMetadata.swift
+++ Sources/Quick/ExampleMetadata.swift
@@ -1,31 +1,34 @@
 import Foundation
 
+// `#if swift(>=3.2) && (os(macOS) || os(iOS) || os(tvOS) || os(watchOS)) && !SWIFT_PACKAGE`
+// does not work as expected.
+#if swift(>=3.2)
+    #if (os(macOS) || os(iOS) || os(tvOS) || os(watchOS)) && !SWIFT_PACKAGE
+    @objcMembers
+    public class _ExampleMetadataBase: NSObject {}
+    #else
+    public class _ExampleMetadataBase: NSObject {}
+    #endif
+#else
+public class _ExampleMetadataBase: NSObject {}
+#endif
+
 /**
     A class that encapsulates information about an example,
     including the index at which the example was executed, as
     well as the example itself.
 */
-final public class ExampleMetadata: NSObject {
+final public class ExampleMetadata: _ExampleMetadataBase {
     /**
         The example for which this metadata was collected.
     */
-    #if (os(macOS) || os(iOS) || os(tvOS) || os(watchOS)) && !SWIFT_PACKAGE
-    @objc
     public let example: Example
-    #else
-    public let example: Example
-    #endif
 
     /**
         The index at which this example was executed in the
         test suite.
     */
-    #if (os(macOS) || os(iOS) || os(tvOS) || os(watchOS)) && !SWIFT_PACKAGE
-    @objc
     public let exampleIndex: Int
-    #else
-    public let exampleIndex: Int
-    #endif
 
     internal init(example: Example, exampleIndex: Int) {
         self.example = example
