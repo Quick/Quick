afa21ac2ff1768270af33211b767feaf52f9b96e
Sho Ikeda <suicaicoca@gmail.com>
2017-09-05 09:07:17 +0900

    Split `swift(>=3.2)` condition and `(os(macOS) || os(iOS) || os(tvOS) || os(watchOS)) && !SWIFT_PACKAGE` condition

--- Sources/Quick/Callsite.swift
+++ Sources/Quick/Callsite.swift
@@ -1,8 +1,14 @@
 import Foundation
 
-#if swift(>=3.2) && (os(macOS) || os(iOS) || os(tvOS) || os(watchOS)) && !SWIFT_PACKAGE
-@objcMembers
-public class _CallsiteBase: NSObject {}
+// `#if swift(>=3.2) && (os(macOS) || os(iOS) || os(tvOS) || os(watchOS)) && !SWIFT_PACKAGE`
+// does not work as expected.
+#if swift(>=3.2)
+    #if (os(macOS) || os(iOS) || os(tvOS) || os(watchOS)) && !SWIFT_PACKAGE
+    @objcMembers
+    public class _CallsiteBase: NSObject {}
+    #else
+    public class _CallsiteBase: NSObject {}
+    #endif
 #else
 public class _CallsiteBase: NSObject {}
 #endif
