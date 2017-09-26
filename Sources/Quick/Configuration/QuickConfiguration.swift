264c02083897dd348d56abe9037ac2f24efbeef1
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2017-09-26 23:19:18 +0200

    Replace guard let with if to make more readable getting the superclass

--- Sources/Quick/Configuration/QuickConfiguration.swift
+++ Sources/Quick/Configuration/QuickConfiguration.swift
@@ -26,10 +26,9 @@ internal func qck_enumerateSubclasses<T: AnyObject>(_ klass: T.Type, block: (T.T
     for i in 0..<classesCount {
         subclass = classes[Int(i)]
 
-        guard let superclass = class_getSuperclass(subclass),
-            superclass === klass else { return }
-
-        block(subclass as! T.Type) // swiftlint:disable:this force_cast
+        if let superclass = class_getSuperclass(subclass), superclass === klass {
+            block(subclass as! T.Type) // swiftlint:disable:this force_cast
+        }
     }
 
     free(classes)
