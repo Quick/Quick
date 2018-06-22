8188ee402a7f144be6cc6bfb10a3d276e2707611
Sho Ikeda <suicaicoca@gmail.com>
2018-06-22 14:35:39 +0900

    Update documentation comments

--- Sources/Quick/NSString+C99ExtendedIdentifier.swift
+++ Sources/Quick/NSString+C99ExtendedIdentifier.swift
@@ -21,6 +21,8 @@ extension NSString {
         return invalidCharacters
     }()
 
+    /// This API is not meant to be used outside Quick, so will be unavaialbe in
+    /// a next major version.
     @objc(qck_c99ExtendedIdentifier)
     public var c99ExtendedIdentifier: String {
         let validComponents = components(separatedBy: NSString.invalidCharacters)
@@ -30,10 +32,11 @@ extension NSString {
     }
 }
 
-/// Extension methods or properties for NSObject subclasses are somehow invisible
-/// from the Objective-C runtime on static linking, so let's make a wrapper class.
+/// Extension methods or properties for NSObject subclasses are invisible from
+/// the Objective-C runtime on static linking unless the consumers add `-ObjC`
+/// linker flag, so let's make a wrapper class to mitigate that situation.
 ///
-/// See: https://github.com/Quick/Quick/issues/785
+/// See: https://github.com/Quick/Quick/issues/785 and https://github.com/Quick/Quick/pull/803
 @objc
 class QCKObjCStringUtils: NSObject {
     override private init() {}
