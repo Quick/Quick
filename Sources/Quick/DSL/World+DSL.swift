28666c772f1dcd59d5c7919e5c2b875ed5329a32
Paul Cantrell <cantrell@pobox.com>
2020-04-09 22:12:21 -0500

    Fixed aroundEach on Linux
    Replicated non-check for proper context from beforeEach and afterEach

--- Sources/Quick/DSL/World+DSL.swift
+++ Sources/Quick/DSL/World+DSL.swift
@@ -92,13 +92,16 @@ extension World {
         currentExampleGroup.hooks.appendAround(closure)
     }
 
+#if canImport(Darwin)
     @objc(aroundEachWithMetadata:)
     internal func aroundEach(_ closure: @escaping AroundExampleWithMetadataClosure) {
-        guard currentExampleMetadata == nil else {
-            raiseError("'aroundEach' cannot be used inside '\(currentPhase)', 'aroundEach' may only be used inside 'context' or 'describe'. ")
-        }
         currentExampleGroup.hooks.appendAround(closure)
     }
+#else
+    internal func aroundEach(_ closure: @escaping AroundExampleWithMetadataClosure) {
+        currentExampleGroup.hooks.appendAround(closure)
+    }
+#endif
 
     @nonobjc
     internal func it(_ description: String, flags: FilterFlags, file: FileString, line: UInt, closure: @escaping () -> Void) {
