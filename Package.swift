1d156b195c9511f03442ac92577c96b32e494f9f
IKEDA Sho <suicaicoca@gmail.com>
2020-09-17 20:57:01 +0900

    [BREAKING] Bump minimum iOS version to 9.0
    Mainly for Xcode 12 compatibility.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Quick",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9)
+        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9)
     ],
     products: [
         .library(name: "Quick", targets: ["Quick"]),
