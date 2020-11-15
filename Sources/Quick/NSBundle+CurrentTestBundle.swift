d7b23e7fb2362c5b54359c1330f368decf51f895
Ryan Baumbach <github@ryan.codes>
2020-11-15 12:00:18 -0700

    Make NSBundle.currentTestBundle() public

--- Sources/Quick/NSBundle+CurrentTestBundle.swift
+++ Sources/Quick/NSBundle+CurrentTestBundle.swift
@@ -7,7 +7,7 @@ extension Bundle {
     /**
      Locates the first bundle with a '.xctest' file extension.
      */
-    internal static var currentTestBundle: Bundle? {
+    public static var currentTestBundle: Bundle? {
         return allBundles.first { $0.bundlePath.hasSuffix(".xctest") }
     }
 
