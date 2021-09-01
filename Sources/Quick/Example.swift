486331d26779c1eb1dd7d7637252f94e7e1d9487
Alexander Momchilov <alexandermomchilov@gmail.com>
2021-09-01 16:14:21 -0400

    Report skipped tests to Xcode

--- Sources/Quick/Example.swift
+++ Sources/Quick/Example.swift
@@ -62,7 +62,7 @@ final public class Example: _ExampleBase {
         Executes the example closure, as well as all before and after
         closures defined in the its surrounding example groups.
     */
-    public func run() { // swiftlint:disable:this function_body_length
+    public func run() {
         let world = World.sharedWorld
 
         if world.numberOfExamplesRun == 0 {
@@ -83,31 +83,11 @@ final public class Example: _ExampleBase {
             do {
                 try closure()
             } catch {
-                let description = "Test \(name) threw unexpected error: \(error.localizedDescription)"
-                #if SWIFT_PACKAGE
-                let file = callsite.file.description
-                #else
-                let file = callsite.file
-                #endif
-
-                // XCTIssue is unavailable (not implemented yet) on swift-corelibs-xctest (for non-Apple platforms)
-                #if canImport(Darwin)
-                let location = XCTSourceCodeLocation(filePath: file, lineNumber: Int(callsite.line))
-                let sourceCodeContext = XCTSourceCodeContext(location: location)
-                let issue = XCTIssue(
-                    type: .thrownError,
-                    compactDescription: description,
-                    sourceCodeContext: sourceCodeContext
-                )
-                QuickSpec.current.record(issue)
-                #else
-                QuickSpec.current.recordFailure(
-                    withDescription: description,
-                    inFile: file,
-                    atLine: Int(callsite.line),
-                    expected: false
-                )
-                #endif
+                if let testSkippedError = error as? XCTSkip {
+                    self.reportSkippedTest(testSkippedError, name: name, callsite: callsite)
+                } else {
+                    self.reportFailedTest(error, name: name, callsite: callsite)
+                }
             }
 
             self.group!.phase = .aftersExecuting
@@ -140,6 +120,103 @@ final public class Example: _ExampleBase {
             aggregateFlags[key] = value
         }
         return aggregateFlags
+    }
+
+    private func reportSkippedTest(_ testSkippedError: XCTSkip, name: String, callsite: Callsite) { // swiftlint:disable:this function_body_length
+        #if !canImport(Darwin)
+            return // This functionality is only supported by Apple's proprietary XCTest, not by swift-corelibs-xctest
+        #else // `NSSelectorFromString` requires the Objective-C runtime, which is not available on Linux.
+
+            let messageSuffix = """
+                \n
+                If nobody else has done so yet, please submit an issue to https://github.com/Quick/Quick/issues
+
+                For now, we'll just benignly ignore skipped tests.
+            """
+
+            guard let testRun = QuickSpec.current.testRun else {
+                print("""
+                     [Quick Warning]: `QuickSpec.current.testRun` was unexpectededly `nil`.
+                """ + messageSuffix)
+                return
+            }
+
+            let selector = NSSelectorFromString("recordSkipWithDescription:sourceCodeContext:")
+
+            guard testRun.responds(to: selector) else {
+                print("""
+                [Quick Warning]: The internals of Apple's XCTestCaseRun have changed, as it no longer responds to
+                    the -[XCTSkip \(NSStringFromSelector(selector))] message necessary to report skipped tests to Xcode.
+                """ + messageSuffix)
+               return
+            }
+
+            guard let skippedTestContextAny = testSkippedError.errorUserInfo["XCTestErrorUserInfoKeySkippedTestContext"] else {
+                print("""
+                [Quick Warning]: The internals of Apple's XCTestCaseRun have changed.
+                    We expected the `errorUserInfo` dictionary of the XCTSKip error to contain a value for the key
+                    "XCTestErrorUserInfoKeySkippedTestContext", but it didn't.
+                """ + messageSuffix)
+                return
+            }
+
+            // Uses an internal type "XCTSkippedTestContext", but "NSObject" will be sufficient for `perform(_:with:_with:)`.
+            guard let skippedTestContext = skippedTestContextAny as? NSObject else {
+                print("""
+                [Quick Warning]: The internals of Apple's XCTestCaseRun have changed.
+                    We expected `skippedTestContextAny` to have type `NSObject`,
+                    but we got an object of type \(type(of: skippedTestContextAny))
+                """ + messageSuffix)
+                return
+            }
+
+            guard let sourceCodeContextAny = skippedTestContext.value(forKey: "sourceCodeContext") else {
+                print("""
+                [Quick Warning]: The internals of Apple's XCTestCaseRun have changed.
+                    We expected `XCTSkippedTestContext` to have a `sourceCodeContext` property, but it did not.
+                """ + messageSuffix)
+                return
+            }
+
+            guard let sourceCodeContext = sourceCodeContextAny as? XCTSourceCodeContext else {
+                print("""
+                    [Quick Warning]: The internals of Apple's XCTestCaseRun have changed.
+                    We expected `XCTSkippedTestContext.sourceCodeContext` to have type `XCTSourceCodeContext`,
+                    but we got an object of type \(type(of: sourceCodeContextAny)).
+                """ + messageSuffix)
+                return
+            }
+
+            testRun.perform(selector, with: testSkippedError.message, with: sourceCodeContext)
+        #endif
+    }
+
+    private func reportFailedTest(_ error: Error, name: String, callsite: Callsite) {
+        let description = "Test \(name) threw unexpected error: \(error.localizedDescription)"
+
+        #if SWIFT_PACKAGE
+            let file = callsite.file.description
+        #else
+            let file = callsite.file
+        #endif
+
+        #if !SWIFT_PACKAGE
+            let location = XCTSourceCodeLocation(filePath: file, lineNumber: Int(callsite.line))
+            let sourceCodeContext = XCTSourceCodeContext(location: location)
+            let issue = XCTIssue(
+                type: .thrownError,
+                compactDescription: description,
+                sourceCodeContext: sourceCodeContext
+            )
+            QuickSpec.current.record(issue)
+        #else
+            QuickSpec.current.recordFailure(
+                withDescription: description,
+                inFile: file,
+                atLine: Int(callsite.line),
+                expected: false
+            )
+        #endif
     }
 }
 
