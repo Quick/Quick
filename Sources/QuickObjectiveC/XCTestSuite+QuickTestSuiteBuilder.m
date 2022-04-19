02edfea48bb4ad60b43a72e65890da951bea87ea
Rachel Brindle <you@subluminal.net>
2022-04-19 10:56:15 -0700

    Fix running tests in parallel (#1143)
    * Fix running tests in parallel
    
    This more-or-less reverts 90ec83d37cc8e943944ee07ed0ea9cf2f541c6b1. I did run some manual tests to verify that you can still run tests individually.
    
    * Run some of the test targets in parallel
    
    * Remove tests of testClassSuitesForTestIdentifiers:skippingTestIdentifiers:randomNumberGenerator:
    
    * Run focused tests serially

--- Sources/QuickObjectiveC/XCTestSuite+QuickTestSuiteBuilder.m
+++ Sources/QuickObjectiveC/XCTestSuite+QuickTestSuiteBuilder.m
@@ -20,10 +20,6 @@
     Method testCaseWithName = class_getClassMethod(self, @selector(testSuiteForTestCaseWithName:));
     Method hooked_testCaseWithName = class_getClassMethod(self, @selector(qck_hooked_testSuiteForTestCaseWithName:));
     method_exchangeImplementations(testCaseWithName, hooked_testCaseWithName);
-    
-    Method testClassSuitesForTestIdentifiers = class_getClassMethod(self, NSSelectorFromString(@"testClassSuitesForTestIdentifiers:skippingTestIdentifiers:randomNumberGenerator:"));
-    Method hooked_testClassSuitesForTestIdentifiers = class_getClassMethod(self, @selector(qck_hooked_testClassSuitesForTestIdentifiers:skippingTestIdentifiers:randomNumberGenerator:));
-    method_exchangeImplementations(testClassSuitesForTestIdentifiers, hooked_testClassSuitesForTestIdentifiers);
 }
 
 /**
@@ -45,39 +41,6 @@
 + (nullable instancetype)qck_hooked_testSuiteForTestCaseWithName:(nonnull NSString *)name {
     NSArray<NSString *> *components = [name componentsSeparatedByString:@"/"];
     return [QuickTestSuite selectedTestSuiteForTestCaseWithName:[components firstObject] testName:[components count] > 1 ? [components lastObject] : nil];
-}
-
-/// Starting with Xcode 12.5 XCTest uses `testClassSuitesForTestIdentifiers:` instead of `testSuiteForTestCaseWithName:`
-+ (id)qck_hooked_testClassSuitesForTestIdentifiers:(id)arg1 skippingTestIdentifiers:(id)arg2 randomNumberGenerator:(long long)arg3 {
-    // Create resulting suite
-    XCTestSuite *suite = [[XCTestSuite alloc] initWithName:@"Selected by Quick"];
-    
-    // Enumerating all XCTTestIdentifiers
-    // - `arg1` is _XCTTestIdentifierSet_Set
-    // - `testIdentifier` is _XCTTestIdentifier_Double
-    for (id testIdentifier in arg1) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-        SEL componentsSel = NSSelectorFromString(@"components");
-        NSArray<NSString *> *components = [testIdentifier performSelector:componentsSel];
-#pragma clang diagnostic pop
-
-        NSString *testCaseName = [components firstObject];
-        NSString *testName = nil;
-        if ([components count] > 1) {
-            testName = [components lastObject];
-        }
-        
-        // Get suite for current XCTTestIdentifier
-        QuickTestSuite *quickSuite = [QuickTestSuite selectedTestSuiteForTestCaseWithName:testCaseName testName:testName];
-        
-        // Add all tests from current XCTTestIdentifier to resulting suite
-        for (XCTest *test in quickSuite.tests) {
-            [suite addTest:test];
-        }
-    }
-    
-    return suite;
 }
 
 @end
