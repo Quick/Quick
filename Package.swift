1d08f2264a92115fd8198ca418e798c50b366402
Rachel Brindle <you@subluminal.net>
2023-04-15 07:45:51 -0700

    Update Nimble to v12

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "Quick", targets: ["Quick"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Nimble.git", from: "11.2.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "12.0.0"),
     ],
     targets: {
         var targets: [Target] = [
