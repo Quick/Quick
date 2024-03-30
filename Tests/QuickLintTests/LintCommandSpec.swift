40f229e6847f137f0d2b6359bc873d0cb5b3f362
Rachel Brindle <you@subluminal.net>
2024-03-29 19:45:51 -0700

    Derp. Don't hardcode a path in test.

--- Tests/QuickLintTests/LintCommandSpec.swift
+++ Tests/QuickLintTests/LintCommandSpec.swift
@@ -23,14 +23,14 @@ final class LintCommandSpec: AsyncSpec {
             try await subject.run()
 
             expect(writer.stderrSpy).to(beCalled([
-                "/Users/you/workspace/Quick/Tests/LintFixtures/Nesting/Nesting.swift:6:9: warning: Focused Spec Detected.",
-                "/Users/you/workspace/Quick/Tests/LintFixtures/Nesting/Nesting.swift:7:13: warning: Focused Spec Detected.",
-                "/Users/you/workspace/Quick/Tests/LintFixtures/Nesting/Nesting.swift:10:9: warning: Focused Spec Detected.",
-                "/Users/you/workspace/Quick/Tests/LintFixtures/Nesting/Nesting.swift:10:62: warning: Focused Spec Detected.",
-                "/Users/you/workspace/Quick/Tests/LintFixtures/SampleSpec.swift:6:9: warning: Focused Spec Detected.",
-                "/Users/you/workspace/Quick/Tests/LintFixtures/SampleSpec.swift:11:9: warning: Focused Spec Detected.",
-                "/Users/you/workspace/Quick/Tests/LintFixtures/SampleSpec.swift:16:9: warning: Focused Spec Detected.",
-                "/Users/you/workspace/Quick/Tests/LintFixtures/SampleSpec.swift:21:9: warning: Focused Spec Detected."
+                "\(rootURL.path)/Nesting/Nesting.swift:6:9: warning: Focused Spec Detected.",
+                "\(rootURL.path)/Nesting/Nesting.swift:7:13: warning: Focused Spec Detected.",
+                "\(rootURL.path)/Nesting/Nesting.swift:10:9: warning: Focused Spec Detected.",
+                "\(rootURL.path)/Nesting/Nesting.swift:10:62: warning: Focused Spec Detected.",
+                "\(rootURL.path)/SampleSpec.swift:6:9: warning: Focused Spec Detected.",
+                "\(rootURL.path)/SampleSpec.swift:11:9: warning: Focused Spec Detected.",
+                "\(rootURL.path)/SampleSpec.swift:16:9: warning: Focused Spec Detected.",
+                "\(rootURL.path)/SampleSpec.swift:21:9: warning: Focused Spec Detected."
             ]))
         }
     }
